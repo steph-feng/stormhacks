323ecd4049dcf3112504780e3ccb5d9717b6df9f
Stephanie Feng <stephanie.c.feng@gmail.com>
2024-10-06 02:23:51 -0700

    improve colony detection

--- backend/project/image_processing/models.py
+++ backend/project/image_processing/models.py
@@ -14,18 +14,16 @@ class ProcessImage (models.Model):
     def count(self):
         current = cv2.imread(self.image.path)
         current_grey = cv2.cvtColor(current, cv2.COLOR_BGR2GRAY)
-        current_canny = cv2.Canny(current_grey, 30, 150, 3)
-        (contours, hierarchy) = cv2.findContours(
-            current_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
-        )
+        current_blur = cv2.medianBlur(current_grey, 3)
+        current_canny = cv2.Canny(current_blur, 30, 150, 3)
+        (contours, hierarchy) = cv2.findContours(current_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
         self.num_colonies = len(contours)
-        cv2.drawContours(current, contours, -1, (255, 0, 0), 2)
+        cv2.drawContours(current, contours, -1, (0, 0, 255), 1)
 
         contour_image_filename = f'{self.image.name}_contour'
         contour_image_path = os.path.join(settings.MEDIA_ROOT, contour_image_filename)
         cv2.imwrite(f'{contour_image_path}.jpg', current)
-
         contour_image_url = f'{settings.MEDIA_URL}{contour_image_filename}.jpg'
         return contour_image_url
 
